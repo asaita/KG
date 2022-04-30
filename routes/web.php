273bd7caaeeecf96bcf39ceaac4bc2905b8ed080
asaita <saitayyildiz@gmail.com>
2022-05-01 01:02:22 +0300

    odeme controller

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,4 @@ Route::view('/anasayfa','anasayfa');
 Route::get('/kategori/{slug_kategoriadi}','App\Http\Controllers\kategoriController@index')->name('kategori');
 Route::get('/urun/{slug_urunadi}','App\Http\Controllers\urunController@index')->name('urun');
 Route::get('/sepet','App\Http\Controllers\sepetController@index')->name('sepet');
+Route::get('/odeme','App\Http\Controllers\odemeController@index')->name('odeme');
